6420ee6ba032a66c949b7fca56ad633efc3294d6
Halobrobro <Halobrobro@mail.ru>
2023-02-16 11:49:23 +0700

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from "./component/HelloWorld";
+import Name from "./component/name";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+          <HelloWorld></HelloWorld>
+          <Name></Name>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
